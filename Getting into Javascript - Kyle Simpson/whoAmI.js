700295d9209152511f249941c104c4e4fcc3b09a
Emily Richardson <richardson96@gmail.com>
2021-01-27 23:37:01 -0600

    completed favoriteBook functions

---
+++ Getting into Javascript - Kyle Simpson/whoAmI.js
@@ -0,0 +1,12 @@
+let emergingDeveloper = "Emily";
+let twitterHandle = "em_rich3";
+let age = 24;
+
+function whoAmI(myName, myNickname, myAge) {
+    console.log(`
+    Hi, I'm ${myName} (aka ${myNickname}),
+     and I'm ${myAge} years old.`
+     );
+}
+
+whoAmI(emergingDeveloper, twitterHandle, age);
