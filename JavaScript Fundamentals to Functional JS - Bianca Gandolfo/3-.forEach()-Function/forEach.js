40cc9b9425bce02189941b16cf98b54b10a3e36f
Emily Richardson <richardson96@gmail.com>
2021-02-16 00:04:11 -0600

    created _.each function

--- JavaScript Fundamentals to Functional JS - Bianca Gandolfo/3-.forEach()-Function/forEach.js
+++ JavaScript Fundamentals to Functional JS - Bianca Gandolfo/3-.forEach()-Function/forEach.js
@@ -10,4 +10,25 @@ function createSuspectObject(name) {
 
 let suspects = ["Miss Scarlet", "Colonel Mustanrd", "Mr. White"];
 
+const _ = {};
+_.each = function(list, callback) {
+    if (Array.isArray(list)) {
+        for (let i = 0; i < list.length; i++) {
+            callback(list[i], i, list);
+        }
+    } else {
+        for (let key in list) {
+            callback(list[key], key, list)
+        }
 
+    }
+
+}
+
+_.each(['sally', 'georgie', 'pete'], function(name, i, list) {
+    if (name[i+1]) {
+        console.log(name, 'is younger than', name[i+1])
+    } else {
+        console.log(name, 'is the oldest')
+    }
+})
